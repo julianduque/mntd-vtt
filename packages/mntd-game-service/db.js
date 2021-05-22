faad932159dc730e53d45e658bc539d2dba20880
Cabu Vallejos <cabupy@gmail.com>
2021-05-22 00:31:51 -0400

    Agregamos a db mas cases

--- packages/mntd-game-service/db.js
+++ packages/mntd-game-service/db.js
@@ -1,40 +1,60 @@
-const pg = require('pg')
+const pg = require("pg");
 
 const postgres = {
-  database: process.env.PG_DB ?? 'mntd',
-  host: process.env.PG_HOST ?? 'localhost',
-  port: process.env.PG_PORT ?? '5432',
-  user: process.env.PG_USER ?? 'postgres',
-  password: process.env.PG_PASS ?? 'postgres'
-}
+  database: process.env.PG_DB ?? "mntd",
+  host: process.env.PG_HOST ?? "localhost",
+  port: process.env.PG_PORT ?? "5432",
+  user: process.env.PG_USER ?? "postgres",
+  password: process.env.PG_PASS ?? "postgres",
+};
 
 module.exports = {
-  query: strSQL => {
+  query: (strSQL) => {
     return new Promise((resolve, reject) => {
-      const client = new pg.Client(postgres)
-      client.connect(err => {
+      const client = new pg.Client(postgres);
+      client.connect((err) => {
         if (err) {
-          console.log(err)
-          reject(err)
+          console.log(err);
+          reject(err);
         } else {
           client.query(strSQL, (err, result) => {
             if (err) {
-              reject(err)
+              reject(err);
             } else {
+              let res = { rows: [] };
               switch (result.command) {
+                case 'CREATE':
+                  resolve(result.command)
                 case 'SELECT':
-                  resolve(result.rows)
-                  break
-                // Despues trataremos el INSERT, UPDATE, DELETE y demas.
+                  resolve(result.rows);
+                  break;
+                case 'INSERT':
+                  if (result.rowCount > 0) {
+                    res.rows = result.rows;
+                  }
+                  resolve(res);
+                  break;
+                case 'UPDATE':
+                  if (result.rowCount > 0) {
+                    res.rows = result.rows;
+                  }
+                  resolve(res);
+                  break;
+                case 'DELETE':
+                  if (result.rowCount > 0) {
+                    res.rows = result.rows;
+                  }
+                  resolve(res);
+                  break;
                 default:
-                  resolve(result)
-                  break
+                  resolve(result);
+                  break;
               }
-              client.end()
+              client.end();
             }
-          })
+          });
         }
-      })
-    })
+      });
+    });
   },
-}
+};
